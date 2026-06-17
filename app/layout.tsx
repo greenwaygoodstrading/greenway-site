96ba5f24b93e18c0c5635f796362ea7f7c1b0679
Yeswanth Ram <your_github_email@example.com>
2026-06-17 13:44:23 +0530

    added-google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type {Metadata} from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Cormorant_Garamond, DM_Sans, Hind_Madurai } from 'next/font/google';
 
@@ -32,6 +33,17 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className={`${cormorantGaramond.variable} ${dmSans.variable} ${hindMadurai.variable}`}>
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-F4EP371QTV" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F4EP371QTV');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning>{children}</body>
     </html>
   );
